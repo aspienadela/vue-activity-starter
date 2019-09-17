b25e6c73f806d789bbb780a488faf9b36d8026d0
aspienadela <54401524+aspienadela@users.noreply.github.com>
2019-09-17 04:31:59 -0400

    added new files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,247 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="vueGoalApp">
+    <nav class="navbar is-white topNav">
+      <div class="container">
+        <div class="navbar-brand">
+          <h1>{{ fullAppName }}</h1>
+        </div>
+      </div>
+    </nav>
+    <nav class="navbar is-white">
+      <div class="container">
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a
+              class="navbar-item is-active"
+              href="#"
+            >Newest</a>
+            <a
+              class="navbar-item"
+              href="#"
+            >In Progress</a>
+            <a
+              class="navbar-item"
+              href="#"
+            >Finished</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="container">
+      <div class="columns">
+        <div class="column is-3">
+          <a
+            v-if="!isFormDisplayed"
+            class="button is-primary is-block is-alt is-large"
+            href="#"
+            @click="toggleFormDisplay"
+          >New Activity</a>
+          <div
+            v-if="isFormDisplayed"
+            class="create-form"
+          >
+            <h2>Create Activity</h2>
+            <form>
+              <div class="field">
+                <label class="label">Title</label>
+                <div class="control">
+                  <input
+                    v-model="newActivity.title"
+                    class="input"
+                    type="text"
+                    placeholder="Read a Book"
+                  >
+                </div>
+              </div>
+              <div class="field">
+                <label class="label">Notes</label>
+                <div class="control">
+                  <textarea
+                    v-model="newActivity.notes"
+                    class="textarea"
+                    placeholder="Write some notes here"
+                  />
+                </div>
+              </div>
+              <div class="field is-grouped">
+                <div class="control">
+                  <button
+                    type="button"
+                    class="button is-link"
+                    :disabled="!isFormValid"
+                    @click="createActivity"
+                  >
+                    Create Activity
+                  </button>
+                </div>
+                <div class="control">
+                  <button
+                    type="button"
+                    class="button is-text"
+                    @click="toggleFormDisplay"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="column is-9">
+          <div class="box content">
+            <ActivityItem
+              v-for="activity in activities"
+              :key="activity.id"
+              :activity="activity"
+            />
+            <div class="activity-length">Currently have {{ activityCount }} activities</div>
+            <div class="activity-motivation">Activity motivation</div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ActivityItem from './components/ActivityItem'
+import { ActivitiesAPI, UserAPI, CategoriesAPI } from './api'
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    ActivityItem
+  },
+  data() {
+    return {
+      isFormDisplayed: false,
+      creator: 'Pjay Nadela',
+      appName: 'Activity Planner',
+      newActivity: {
+        id: Math.random().toString(10).substring(8),
+        title: '',
+        notes: ''
+      },
+      items: {1: {name: 'Pjay'}, 2: {name: 'Nadela'}},
+      user: {
+        name: 'Pjay Nadela',
+        id: '-Aj34jknvncx98812',
+      },
+      activities: {},
+      categories: {
+        '1546969049': {text: 'books'},
+        '1546969225': {text: 'movies'}
+      }
+    }
+  },
+  computed: {
+    isFormValid () {
+      return this.newActivity.title && this.newActivity.notes
+    },
+    fullAppName(){
+      return `${this.appName} by ${this.creator}`
+    },
+    activityCount(){
+      return Object.keys(this.activities).length
+    }
+  },  
+  created() {
+    this.activities = ActivitiesAPI()
+    this.user = UserAPI()
+    this.categories = CategoriesAPI()
+  },
+  methods: {
+    toggleFormDisplay () {
+      this.isFormDisplayed = !this.isFormDisplayed
+    },
+    createActivity() {
+      let activity = new Object
+      let id = this.newActivity.id
+        activity = this.newActivity
+        this.activities[id] = activity
+        console.log(this.activities)
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#vueGoalApp {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+html,body {
+  font-family: 'Open Sans', serif;
+  background: #F2F6FA;
+}
+footer {
+  background-color: #F2F6FA !important;
+}
+.example-wrapper {
+  margin-left: 30px;
+}
+.topNav {
+  border-top: 5px solid #3498DB;
+}
+.topNav .container {
+  border-bottom: 1px solid #E6EAEE;
+}
+.container .columns {
+  margin: 3rem 0;
+}
+.navbar-menu .navbar-item {
+  padding: 0 2rem;
+}
+aside.menu {
+  padding-top: 3rem;
+}
+aside.menu .menu-list {
+  line-height: 1.5;
+}
+aside.menu .menu-label {
+  padding-left: 10px;
+  font-weight: 700;
+}
+.button.is-primary.is-alt {
+  background: #00c6ff;
+  background: -webkit-linear-gradient(to bottom, #0072ff, #00c6ff);
+  background: linear-gradient(to bottom, #0072ff, #00c6ff);
+  font-weight: 700;
+  font-size: 14px;
+  height: 3rem;
+  line-height: 2.8;
+}
+.media-left img {
+  border-radius: 50%;
+}
+.media-content p {
+  font-size: 14px;
+  line-height: 2.3;
+  font-weight: 700;
+  color: #8F99A3;
+}
+article.post {
+  margin: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #E6EAEE;
+}
+article.post:last-child {
+  padding-bottom: 0;
+  border-bottom: none;
+}
+.activity-length {
+  display: inline-block;
+}
+.activity-motivation {
+  float: right;
+}
+
+.menu-list li{
+  padding: 5px;
+}
+
+.navbar-brand > h1 {
+  font-size: 31px;
+  padding: 20px;
 }
 </style>
